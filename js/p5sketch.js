7478cb86448c2caf843ec3c58abc75b3e0a852d8
terrycunniff <teresacunniff@gmail.com>
2018-05-11 11:25:59 -0700

    update

---
+++ js/p5sketch.js
@@ -0,0 +1,21 @@
+
+var canvas;
+
+function windowResized(){
+	resizeCanvas();
+}
+
+function setup() {
+  canvas = createCanvas (windowWidth, windowHeight);
+  canvas.position (0, 0);
+  canvas.style ('z-index', '-1');
+
+  noStroke();
+  fill(143, 66, 244,150);
+	
+}
+
+function draw() {
+  ellipse (mouseX, mouseY, 20, 20);
+
+}
